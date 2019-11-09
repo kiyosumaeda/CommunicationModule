ff352bb766f0909d770956d70d5a1e4d95b1fb47
kiyosu.maeda <kiyosu.0407@gmail.com>
2019-11-09 14:04:09 +0900

    udp_client for python raspberry pi

--- Rasp/src/udp_client.py
+++ Rasp/src/udp_client.py
@@ -1,1 +1,36 @@
-from socket import socket, AF_INET, SOCK_DGRAM
+import threading
+import time
+from socket import *
+
+class ServerThread(threading.Thread):
+    def __init__(self, PORT=7777):
+        threading.Thread.__init__(self)
+        self.data = 'hoge'
+        self.kill_flag = False
+        # line information
+        self.HOST = gethostname()
+        self.PORT = PORT
+        self.BUFSIZE = 1024
+        self.ADDR = (gethostbyname(self.HOST), self.PORT)
+        # bind
+        self.udpServSock = socket(AF_INET, SOCK_DGRAM)
+        self.udpServSock.bind(self.ADDR) # HOST, PORTでbinding
+
+    def run(self):
+        while True:
+            try:
+                data, self.addr = self.udpServSock.recvfrom(self.BUFSIZE) # データ受信
+                self.data = data.decode()
+            except:
+                pass
+
+if __name__ == '__main__':
+    th = ServerThread()
+    th.setDaemon(True)
+    th.start()
+
+    while True:
+        if not th.data:
+            break
+        print(th.data)
+        time.sleep(0.1)
