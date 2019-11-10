575ce486763c1ce09a730fc1a548dbbd976198b4
kiyosu.maeda <kiyosu.0407@gmail.com>
2019-11-10 12:39:26 +0900

    add udp receiver

---
+++ Rasp/src/udp_receiver.py
@@ -0,0 +1,13 @@
+import socket
+import struct
+from contextlib import closing
+
+UDP_IP = ""
+UDP_PORT = 7777
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+with closing(sock):
+	while True:
+		data, addr = sock.recvfrom(1024)
+		print("bytes: ", data)
+		print(str(struct.unpack('>d', data[0])))
