f22867b6f0bf23cb2cdde405017994f5a0300175
kiyosu.maeda <kiyosu.0407@gmail.com>
2020-03-10 13:54:16 +0900

    fix error

--- Rasp/src/udp_receiver.py
+++ Rasp/src/udp_receiver.py
@@ -3,11 +3,11 @@ import struct
 from contextlib import closing
 
 UDP_IP = ""
-UDP_PORT = 7777
+UDP_PORT = 50006
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 with closing(sock):
 	while True:
 		data, addr = sock.recvfrom(1024)
 		print("bytes: ", data)
-		print(str(struct.unpack('>d', data)[0]))
+		# print(str(struct.unpack('>d', data)[0]))
