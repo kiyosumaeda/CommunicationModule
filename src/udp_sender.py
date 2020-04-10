01ae813b48a81f031d33b99a02463ac94ea4be31
kiyosu.maeda <kiyosu.0407@gmail.com>
2020-04-10 21:33:27 +0900

    fix sender error

--- src/udp_sender.py
+++ src/udp_sender.py
@@ -3,9 +3,11 @@ import socket
 class UDPSender:
 	def __init__(self, ip, port):
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.ip = ip
+		self.port = port
 
 	def send(self, msg):
-		self.socket.sendto(msg.encode(), (self.ip, self.port))
+		self.sock.sendto(msg.encode(), (self.ip, self.port))
 
 	def close(self):
 		self.sock.close()
